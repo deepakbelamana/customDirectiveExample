a0ee8c3bbb96184f4c4743769f9be4b94b0e195a
deepak belamana <saideepak627@gmail.com>
2024-04-01 12:10:20 +0530

    custom structural directive added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { EvenNumberHighLighterDirective } from './even-number-high-lighter.directive';
 import { OddNumberStylerDirective } from './odd-number-styler.directive';
+import { OddNumberDirectiveDirective } from './odd-number-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     EvenNumberHighLighterDirective,
     OddNumberStylerDirective,
+    OddNumberDirectiveDirective,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/odd-number-directive.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appOddNumberDirective]'
+})
+export class OddNumberDirectiveDirective {
+
+  @Input() set appOddNumberDirective (condition : boolean) {
+     if(condition) {
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
+     } else {
+      this.viewContainerRef.clear();
+     }
+  }
+
+
+  constructor(private templateRef:TemplateRef<any> , private viewContainerRef : ViewContainerRef) { }
+
+}
